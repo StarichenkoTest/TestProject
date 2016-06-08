5ac9dae1b4528c4e2bf61043b6c8ba270290afda
wownetort <fauust@gmail.com>
2016-06-08 15:38:13 +0300

    Test project

---
+++ Test/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Test
+{
+    public class Program
+    {
+        public const double Epsilon = 0.0001;
+
+        static public double TriangleArea(double side1, double side2, double side3)
+        {
+            if (double.IsNaN(side1) || side1 < Epsilon || side1 > 1.3407807929942596E+154)
+                throw new ArgumentOutOfRangeException("side1", "Side length must be greater than 0 and less than 1.3407807929942596E+154");
+
+            if (double.IsNaN(side2) || side2 < Epsilon || side2 > 1.3407807929942596E+154)
+                throw new ArgumentOutOfRangeException("side2", "Side length must be greater than 0 and less than 1.3407807929942596E+154");
+
+            if (double.IsNaN(side3) || side3 < Epsilon || side3 > 1.3407807929942596E+154)
+                throw new ArgumentOutOfRangeException("side3", "Side length must be greater than 0 and less than 1.3407807929942596E+154");
+
+            double a;
+            double b;
+            double c;
+            if (side1 < side2)
+            {
+                a = side1;
+                if (side2 < side3)
+                {
+                    b = side2;
+                    c = side3;
+                }
+                else
+                {
+                    b = side3;
+                    c = side2;
+                }
+            }
+            else
+            {
+                a = side2;
+                if (side1 < side3)
+                {
+                    b = side1;
+                    c = side3;
+                }
+                else
+                {
+                    b = side3;
+                    c = side1;
+                }
+            }
+            if (Math.Abs(c - a) < Epsilon || Math.Abs(c - b) < Epsilon || Math.Abs(a*a + b*b - c*c) > Epsilon)
+                throw new ArgumentException("A right-angled triangle can not be built from the data sides.");
+            
+            return b / 2 * a;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ TestTests/ProgramTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test.Tests
+{
+    [TestFixture()]
+    public class ProgramTests
+    {
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaThrowsArgumentOutOfRangeException000()
+        {
+            Program.TriangleArea(0, 0, 0);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaThrowsArgumentOutOfRangeException100()
+        {
+            Program.TriangleArea(1, 0, 0);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaThrowsArgumentOutOfRangeException110()
+        {
+            Program.TriangleArea(1, 1, 0);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaThrowsArgumentOutOfRangeException11NaN()
+        {
+            Program.TriangleArea(1, 1, double.NaN);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaThrowsArgumentOutOfRangeException1NaN1()
+        {
+            Program.TriangleArea(1, double.NaN, 1);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaThrowsArgumentOutOfRangeExceptionNaN11()
+        {
+            Program.TriangleArea(double.NaN, 1, 1);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaThrowsArgumentOutOfRangeExceptionInfinite11()
+        {
+            Program.TriangleArea(double.PositiveInfinity, 1, 1);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaThrowsArgumentOutOfRangeException1MaxValue1()
+        {
+            Program.TriangleArea(1, double.MaxValue, 1);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaThrowsArgumentOutOfRangeException11Max()
+        {
+            Program.TriangleArea(1, 1, 1.3407807929942596E+155);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TriangleAreaThrowsArgumentException111()
+        {
+            Program.TriangleArea(1, 1, 1);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TriangleAreaThrowsArgumentException122()
+        {
+            Program.TriangleArea(1, 2, 2);
+        }
+
+        [Test()]
+        public void TriangleArea345()
+        {
+            var d = Program.TriangleArea(3, 4, 5);
+            Assert.AreEqual(6, d);
+        }
+
+        [Test()]
+        public void TriangleArea354()
+        {
+            var d = Program.TriangleArea(3, 5, 4);
+            Assert.AreEqual(6, d);
+        }
+
+        [Test()]
+        public void TriangleArea543()
+        {
+            var d = Program.TriangleArea(5, 4, 3);
+            Assert.AreEqual(6, d);
+        }
+    }
+}
